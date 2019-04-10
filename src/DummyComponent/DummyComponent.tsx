604da8ba166df128a73f2b8f0b1035a1527a2fe8
Krzysztof Bukowski <kbukowski@sii.pl>
2019-04-10 23:52:49 +0200

    Removed src

--- src/DummyComponent/DummyComponent.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-interface DummyComponentInterface {
-    id: string;
-}
-
-export default (props: DummyComponentInterface) => <div>This is a dummy component #{props.id}</div>
